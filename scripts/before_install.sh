a1a7a4035a0f4edd7db0903bf4f38901dcc5c68e
sanket-cmd <75801412+sanket-cmd@users.noreply.github.com>
2025-07-09 11:02:44 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "==== BeforeInstall: Stopping existing backend app (if running)... ===="
+echo "==== BeforeInstall: Ensuring pm2 is installed... ===="
+if ! command -v pm2 &> /dev/null
+then
+    echo "PM2 not found. Installing..."
+    sudo npm install -g pm2
+fi
+
+echo "==== Stopping existing backend app (if running)... ===="
 pm2 stop backend-app || echo "No running app to stop"
