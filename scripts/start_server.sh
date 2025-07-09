523ef4cc824f37a0e02c85e0d6053eb023dd22d7
sanket-cmd <75801412+sanket-cmd@users.noreply.github.com>
2025-07-09 10:53:07 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,11 @@
-  #!/bin/bash
-pm2 start index.js
-pm2 startup
+#!/bin/bash
+
+echo "==== ApplicationStart: Starting backend app with PM2... ===="
+cd /home/ubuntu/app
+
+# Replace "index.js" with your main backend entry file if different
+pm2 start index.js --name "backend-app"
+
+# Save the PM2 process list to resurrect on reboot
 pm2 save
-sudo systemctl restart nginx
+
